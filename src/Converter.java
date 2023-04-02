e35d33169a45f1b830c864653640ae83048f1911
Roman Borodulin <djboroda98@yandex.ru>
2023-04-02 21:33:19 +0300

    Были изменены числовые литералы на константы

--- src/Converter.java
+++ src/Converter.java
@@ -1,12 +1,15 @@
 class Converter {
     // Преобразования шагов в км и ккал
+    static final double CONVERSION_FACTOR_OF_STEP_TO_KM = 0.00075;
+    static final double CONVERSION_FACTOR_OF_STEP_TO_KCAL = 0.05;
+
     int convertToKm(int steps) {
         // 1 шаг = 75 см
-        return (int) (steps * 0.00075);
+        return (int) (steps * CONVERSION_FACTOR_OF_STEP_TO_KM);
     }
 
     int convertStepsToKilocalories(int steps) {
         // 1 шаг = 0.05 Ккал
-        return (int) (steps * 0.05);
+        return (int) (steps * CONVERSION_FACTOR_OF_STEP_TO_KCAL);
     }
 }
